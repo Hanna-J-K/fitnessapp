d8c08b5d049af4d6c0919eae753d31eddb83fcff
210234 Hanna Kraska <210234@edu.p.lodz.pl>
2021-03-27 19:32:32 +0100

    main window

--- fitness.py
+++ fitness.py
@@ -1,3 +1,15 @@
 import sys
 
 from PyQt5 import QtWidgets, QtGui, QtCore
+
+class MainWindow:
+    def __init__(self):
+        app = QtWidgets.QApplication(sys.argv)
+
+        window = QtWidgets.QMainWindow()
+        window.show()
+
+        sys.exit(app.exec())
+
+
+main = MainWindow()
