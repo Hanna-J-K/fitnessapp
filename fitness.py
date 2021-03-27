02e52f14d8c45cc963734f3524604fdf452312e7
210234 Hanna Kraska <210234@edu.p.lodz.pl>
2021-03-27 23:02:51 +0100

    Co-authored-by: w-lugowska <w-lugowska@users.noreply.github.com>

--- fitness.py
+++ fitness.py
@@ -9,16 +9,33 @@ from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow
 
+def goToMain():
+    widget.setCurrentIndex(0)
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         loadUi("MainMenu.ui", self)
+        self.browse_btn.clicked.connect(self.goToBrowse)
+
+    def goToBrowse(self):
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+class BrowseTrainingsWindow(QMainWindow):
+    def __init__(self):
+        super(BrowseTrainingsWindow, self).__init__()
+        loadUi("BrowseTrainingsWindow.ui", self)
+        self.main_menu_btn.clicked.connect(goToMain)
+    
+
 
 # main
 app = QApplication(sys.argv)
 widget = QtWidgets.QStackedWidget()
 main_window = MainWindow()
+browse_window = BrowseTrainingsWindow()
 widget.addWidget(main_window)
+widget.addWidget(browse_window)
 widget.setFixedHeight(600)
 widget.setFixedWidth(800)
 widget.show()
