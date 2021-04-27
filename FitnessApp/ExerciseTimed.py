23db1ca183226aaac8cb51acebbdbf118b5e451c
210234 Hanna Kraska <210234@edu.p.lodz.pl>
2021-04-27 21:12:51 +0200

    refactored UML

--- FitnessApp/ExerciseTimed.py
+++ FitnessApp/ExerciseTimed.py
@@ -5,3 +5,9 @@ class ExerciseTimed(Exercise):
     def __init__(self, name, exercise_type, sets, reps, time, distance):
         super().__init__(name, exercise_type, sets, reps, time, distance)
 
+    def print_information(self):
+        print("Exercise name: " + self.name)
+        print("Type: " + self.exercise_type)
+        print("Time: " + str(self.time))
+        print("Distance: " + str(self.distance))
+
